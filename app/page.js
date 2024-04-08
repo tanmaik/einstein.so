64cbd5fae016f2bb7222594396e1e80dead68722
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-04-08 17:06:14 -0400

    Add bounce motion to landing page elements

--- app/page.js
+++ app/page.js
@@ -11,7 +11,12 @@ export default function Home() {
   return (
     <div className="flex justify-center">
       <div className="text-center px-8">
-        <div className="flex items-center justify-center gap-1 mt-6">
+        <motion.div
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
+          className="flex items-center justify-center gap-1 mt-6"
+        >
           <Image
             src="/logo.png"
             width={50}
@@ -20,27 +25,33 @@ export default function Home() {
             alt="Einstein logo"
           />
           <p className={`${inter.className} font-semibold`}>Einstein</p>
-        </div>
-        <button className="rounded-full mt-20 text-sm ring-1 ring-gray-400 text-gray-400 font-medium px-2 py-1 cursor-help">
-          Closed beta
-        </button>
-        <h1 className="text-5xl tracking-tight mt-4 font-medium font-serif">
-          Revolutionize the way
-          <br />
-          you research.
-        </h1>
-        <p className="mt-4 leading-tight">
-          From experimentation to publication
-          <br />
-          at the snap of a finger.
-        </p>
-
-        <div className="flex justify-center gap-2 items-center mt-4">
-          <button className="bg-gray-200 text-gray-400 cursor-not-allowed px-2 rounded-md font-medium">
-            Start for free
+        </motion.div>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        >
+          <button className="rounded-full mt-20 text-sm ring-1 ring-gray-400 hover:bg-gray-100 transition-all text-gray-400 font-medium px-2 py-1 cursor-help">
+            Closed beta
           </button>
-          <p className="cursor-not-allowed text-gray-400">Book a demo</p>
-        </div>
+          <h1 className="text-5xl tracking-tight mt-4 font-medium font-serif">
+            Revolutionize the way
+            <br />
+            you research.
+          </h1>
+          <p className="mt-4 leading-tight">
+            From experimentation to publication
+            <br />
+            at the snap of a finger.
+          </p>
+
+          <div className="flex justify-center gap-2 items-center mt-4">
+            <button className="bg-gray-200 text-gray-400 cursor-not-allowed px-2 rounded-md font-medium">
+              Start for free
+            </button>
+            <p className="cursor-not-allowed text-gray-400">Book a demo</p>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
